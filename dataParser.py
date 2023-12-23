0b2a0cfdf82141daf8755bc30fb697966fe3ec5a
Maximilian Schnitt <mx.schnitt@t-online.de>
2023-12-23 21:50:06 +0100

    updated pattern

--- dataParser.py
+++ dataParser.py
@@ -1,16 +1,26 @@
 import os
 import re
+import pandas as pd
 from typing import List, Dict
 
-
+# directory paths
 BASEDIR = os.getcwd()
 DATA = os.path.join(BASEDIR, "statistik-ws-20232024-2.csv")
 
+# REGEX Patterns
 DELIMETER_PATTERN = re.compile(r".*\s*Studienfachbelegung nach Abschlusszielen")
 REMOVE_PATTERN = re.compile(r'(?:\s*"EBERHARD KARLS UNIVERSITÄT"\s*|\s*"72074 Tübingen"\s*)|"Stand der Datenerhebung: \d{2}.\d{2}.\d{4}"|"Wintersemester \d{4} / \d{4}"|"ST.NR.S‐\d{3}‐\d{1,3}"')
 EMPTY_LINE_PATTERN = re.compile(r'""\n')
 PAGE_NUMBER_PATTERN = re.compile(r'" ‐ \d{0,3} ‐"')
 MULTIPLE_BLANKS_PATTERN = re.compile(r"\s{2,}")
+FACHSEMESTER_PATTERN = re.compile(r'Fachsemesterzahlen .+')
+HF_PATTERN = re.compile(r'"Ges\.HF."*')
+NF_PATTERN = re.compile(r'"Ges\.NF.*"')
+
+# Constants
+HEADER = []
+HEADER_FIRST_LEVEL = []
+HEADER_SECOND_LEVEL = []
 
 
 def preprocessData(fileAsStr: str) -> List[str]:
@@ -35,15 +45,32 @@ def preprocessData(fileAsStr: str) -> List[str]:
     return formattedData[1:]
 
 def cleanDataForFaculties(preprocessedData: List[str]) -> List[List[str]]:
+    cleanedData = []
     for facultyData in preprocessedData:
         facultyData = re.sub(MULTIPLE_BLANKS_PATTERN," " ,facultyData)
         facultyData = re.sub(PAGE_NUMBER_PATTERN , "",facultyData)
-        splittedData = [data for data in facultyData.split("\n") if not re.fullmatch(r"(?:\"|\s")", data)]
-        print(splittedData)
+        facultyData = re.sub(FACHSEMESTER_PATTERN,"Fachsemester",facultyData)
+        facultyData = re.sub(HF_PATTERN, "",facultyData)
+        splittedData = [data for data in facultyData.split("\n") if not re.fullmatch(r'(?:\"|\s")', data)]
+        cleanedData.append(splittedData)
+    return cleanedData
 
 
-def createDataDictForPandas(preprocessedData: List[str]) -> Dict[str, List[str]]:
-    pass
+def createPandasStructure(subjectData:List[str]) -> Dict[str, List[str]]:
+    pandasDataFrame = []
+    subject = subjectData[0]
+    totalMainSubject = [data for data in subjectData if re.match(HF_PATTERN, data)]
+    totalMinorSubject = [data for data in subjectData if re.match(NF_PATTERN, data)]
+    
+    
+    return pandasDataFrameStructure
+
+def buildPandasDataFrame(preprocessedData: List[str]):
+    pandasFrameStructures = []
+    for data in preprocessedData:
+        pandasStructure = createPandasStructure(data)
+        pandasFrameStructures.append(pandasStructure)
+    return
 
         
 def parseSubsetData(filename: str) -> Dict[str, List[str]]:
@@ -53,6 +80,8 @@ def parseSubsetData(filename: str) -> Dict[str, List[str]]:
         dataAsString = file.read()
         preprocessedData = preprocessData(dataAsString)
         cleanedFacultyData = cleanDataForFaculties(preprocessedData)
+        buildPandasDataFrame(cleanedFacultyData)
+   
        
         
         
