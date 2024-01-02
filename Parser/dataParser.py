5c31d41a0a36e0c0e87aaa684270e6586a5ebb2f
Maximilian Schnitt <mx.schnitt@t-online.de>
2024-01-02 18:06:59 +0100

    fixed parser for multiple files

--- Parser/dataParser.py
+++ Parser/dataParser.py
@@ -14,12 +14,12 @@ class Parser:
         self.DATA = os.path.join(self.BASEDIR, "Parser" ,"statistik-ws-20232024-2.csv")
         self.OUT_PATH = os.path.join(self.BASEDIR, "data", self.output_file)
         
-        self.loaded_data = self.dataLoader()
+        self.pandasDataAsDict = dict()
         
         # MultiIndex 
-        self.HEADER_FIRST_LEVEL = ["", "", *["WiSe 2005/2006" for _ in range(len(constants.SEMESTERS))]]
-        self.HEADER_SECOND_LEVEL = ["", "HF/NF", *["Fachsemester" for _ in range(len(constants.SEMESTERS))]]
-        self.HEADER_THIRD_LEVEL = ["Subject", "", *constants.SEMESTERS]
+        self.HEADER_FIRST_LEVEL = ["", "", *list(chain(*[[year for _ in range(len(constants.SEMESTERS))] for year in constants.YEARS]))]
+        self.HEADER_SECOND_LEVEL = ["", "HF/NF", *list(chain(*[["Fachsemester" for _ in range(len(constants.SEMESTERS))] for _ in range(len(constants.YEARS))]))]
+        self.HEADER_THIRD_LEVEL = ["Subject", "", *list(chain(*[constants.SEMESTERS for _ in range(len(constants.YEARS))]))]
         self.HEADER = [self.HEADER_FIRST_LEVEL, self.HEADER_SECOND_LEVEL,self.HEADER_THIRD_LEVEL]
 
     def __createMultiIndex(self):
@@ -29,7 +29,7 @@ class Parser:
             pd.MultiIndex: MultiIndex for pandas frame
         """
         return pd.MultiIndex.from_arrays(self.HEADER)
-    
+   
     def __preprocessData(self,fileAsStr: str) -> List[str]:
         """ removes unwanted data from the file
 
@@ -69,6 +69,23 @@ class Parser:
             cleanedData.append(splittedData)
         return cleanedData
 
+    def __addElementsToPandaDataAsDict(self,subject,minor, major):
+        """fills self.pandasDataAsDict with data
+
+        Args:
+            subject (str): subject name
+            minor (List): minor data
+            major (List): major data
+        """
+        if subject in self.pandasDataAsDict.keys():  
+            majorData, minorData = self.pandasDataAsDict[subject]
+            majorData[2].extend(major)
+            minorData[2].extend(minor)
+        else:
+            newData = [[subject,"HF",major], [subject, "NF", minor]]
+            self.pandasDataAsDict[subject] = newData
+            
+        
 
     def __createPandasStructureSingleFaculty(self,subjectData:List[str]) -> Tuple[List[str], List[str]]:
         """build list structure which represents minor and major row in data
@@ -86,20 +103,25 @@ class Parser:
         
         #check for empty main and minor subject
         if totalMainSubject == None or len(totalMainSubject) == 0:
-            totalMainSubject = [0 for _ in range(len(constants.SEMESTERS))]
+            totalMainSubject = ["0" for _ in range(len(constants.SEMESTERS))]
         if totalMinorSubject == None or len(totalMinorSubject) == 0:
-            totalMinorSubject = [0 for _ in range(len(constants.SEMESTERS))]
+            totalMinorSubject = ["0" for _ in range(len(constants.SEMESTERS))]
             
         if len(totalMainSubject) != 14:
-            totalMainSubject.insert(0,0)
+            totalMainSubject.insert(0,"0")
         if len(totalMinorSubject) != 14:
-            totalMinorSubject.insert(0,0)
+            totalMinorSubject.insert(0,"0")
             
-        
+        # each row has to have length 14
         assert len(totalMainSubject) == 14 and len(totalMinorSubject) == 14   
+        
+        self.__addElementsToPandaDataAsDict(subject,totalMinorSubject,totalMainSubject)
+        
             
         return [subject, "HF", *totalMainSubject], [subject, "NF", *totalMinorSubject]
         
+
+
 
     def __createPandasStructureAllFaculties(self,preprocessedData: List[str])->List[List[str]]:
         """create 2d array which contains HF/NF data for each faculty
@@ -115,60 +137,48 @@ class Parser:
         for data in preprocessedData:
             # create row data for each subject
             mainSubjectFrame, minorSubjectFrame = self.__createPandasStructureSingleFaculty(data)
-            
             pandasFrameStructures.append(mainSubjectFrame)
             pandasFrameStructures.append(minorSubjectFrame)
         return pandasFrameStructures
         
-        
-    def __clean_excel_sheet(self, sheet:str):
-        """ remove all data from excel sheet
+    def __constructRowDataFromPandasDataFromDict(self):
+        """creates 2d array from self.pandasDataFromDict
 
-        Args:
-            sheet (str): name of excel sheet
+        Returns:
+            List[List]: 2d list of row data for pandas
         """
-        excel_workbench = openpyxl.load_workbook(self.OUT_PATH)
-        try:
-            excel_workbench.remove(excel_workbench[sheet])
-        except KeyError:
-            pass
-        finally:
-            excel_workbench.create_sheet(sheet)
-            excel_workbench.save(self.OUT_PATH)
-
+        pandasRowData = []
+        for major,minor in self.pandasDataAsDict.values():
+            pandasRowData.append([major[0],major[1],*major[2]])
+            pandasRowData.append([minor[0],minor[1],*minor[2]])
+        return pandasRowData       
             
-    def parseSubsetData(self, file:str, output_sheet:str) :
+    def parseSubsetData(self, files:List[str], output_sheet:str) :
         """ parses buggy csv as text and extracts data to excel
 
         Args:
             output_sheet (str): name of excel sheet
         """
-        
-        self.DATA = os.path.join(self.BASEDIR, "data", "csvFiles", file)
-        
-        multiIndex = self.__createMultiIndex()
-        
-        #self.__clean_excel_sheet(output_sheet)
-
-        with open(self.DATA, "r") as file:
-            dataAsString = file.read()
-            preprocessedData = self.__preprocessData(dataAsString)
-            cleanedFacultyData = self.__cleanDataForFaculties(preprocessedData)
-            pandasData = self.__createPandasStructureAllFaculties(cleanedFacultyData)
-            pandasFrame = pd.DataFrame(pandasData, columns=multiIndex)
-            pandasFrame.to_excel(self.OUT_PATH, sheet_name=output_sheet)
+        for file in files:
+            filename = file.replace("/", "-") + ".csv"
+            self.DATA = os.path.join(self.BASEDIR, "data", "csvFiles", filename)            
+            multiIndex = self.__createMultiIndex()
+            with open(self.DATA, "r") as file:
+                dataAsString = file.read()
+                preprocessedData = self.__preprocessData(dataAsString)
+                cleanedFacultyData = self.__cleanDataForFaculties(preprocessedData)
+                pandasData =  self.__createPandasStructureAllFaculties(cleanedFacultyData)
+            
+        # create pandas frame
+        pandasFrame = pd.DataFrame(self.__constructRowDataFromPandasDataFromDict(), columns=multiIndex)
+        pandasFrame.to_excel(self.OUT_PATH, sheet_name=output_sheet)
         
             
 if __name__ == "__main__":
     csvFilePath = os.path.abspath("/Users/maxschnitt/Documents/AnalysingStudentDevelopment/data/csvFiles")
     output_file = "student_data_per_subject.xlsx"
-    filenames = os.listdir(csvFilePath)
+    filenames = constants.YEARS
+    #os.listdir(csvFilePath)
     
     myParser = Parser(output_file)
-    for number, file in enumerate(filenames):
-        print("#######################################")
-        print(f"Currently at file: {file}")
-        sheet = f"Sheet{number}"
-        myParser.parseSubsetData(file, sheet)
-        print("#######################################")
-    #myParser.parseSubsetData("ss-2006.csv", "Sheet1")
+    myParser.parseSubsetData(filenames, "Sheet1")
