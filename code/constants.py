07b9ec4bea689be66e1facfafac24ae5434a6e3c
Maximilian Schnitt <mx.schnitt@t-online.de>
2024-01-02 20:39:32 +0100

    added path constants for saving images

--- code/constants.py
+++ code/constants.py
@@ -1,6 +1,7 @@
 # Description: Constants used in the code
-
 import os
 
+BASE_DIR = os.path.abspath("/Users/maxschnitt/Documents/AnalysingStudentDevelopment")
 PROJECT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 GRADUATES_DIR = os.path.join(PROJECT_DIR, "data/graduates_germany.csv")
+PLOT_DIR = os.path.join(BASE_DIR, "plots")
