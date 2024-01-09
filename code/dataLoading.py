c284fe96fdafccfdeccca2c68a52f3170052bd77
Paul Kaifler <paul@kaifler.eu>
2024-01-09 14:12:10 +0100

    Import helper functions

---
+++ code/dataLoading.py
@@ -0,0 +1,47 @@
+"""Utilities to load data into a pandas DataFrame."""
+
+import pandas as pd
+from constants import GRADUATES_PATH, STUDENTS_PATH, SALLARY_PATH
+
+
+def getStudents() -> pd.DataFrame:
+    """Load student data from excel file into a pandas DataFrame."""
+
+    students = pd.read_excel(STUDENTS_PATH,
+                             sheet_name='Sheet1',
+                             index_col=[0, 1, 2],
+                             header=[0, 2],)
+    students.index = students.index.droplevel(0)
+    return students
+
+
+def getGraduates() -> pd.DataFrame:
+    """Load graduates data from csv file into a pandas DataFrame."""
+
+    graduates = pd.read_csv(
+        GRADUATES_PATH,
+        encoding="ISO-8859-1",
+        sep=";",
+        decimal=".",
+        skiprows=4,
+        header=[1, 2, 3],
+        skipfooter=3,
+        index_col=[0, 1],
+        engine="python")
+    return graduates
+
+
+def getSallaries() -> pd.DataFrame:
+    """Load sallary data from csv file into a pandas DataFrame."""
+
+    sallaries = pd.read_csv(
+        SALLARY_PATH,
+        encoding="ISO-8859-1",
+        sep=";",
+        decimal=".",
+        skiprows=5,
+        header=[0, 1, 2, 3],
+        skipfooter=4,
+        index_col=[0, 1, 2, 3],
+        engine="python")
+    return sallaries
