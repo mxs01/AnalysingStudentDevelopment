0a6ad4573740472dc60a06045f52781d24f3fbb0
Paul Kaifler <paul@kaifler.eu>
2024-01-14 11:59:18 +0100

    update data path

--- src/constants.py
+++ src/constants.py
@@ -4,7 +4,7 @@ import re
 
 # paths
 PROJECT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-DATA_DIR = os.path.join(PROJECT_DIR, "data")
+DATA_DIR = os.path.join(PROJECT_DIR, "dat")
 GRADUATES_PATH = os.path.join(DATA_DIR, "graduates_germany.csv")
 SALLARY_PATH = os.path.join(DATA_DIR, "sallary_per_sector.csv")
 STUDENTS_PATH = os.path.join(DATA_DIR, "student_data_per_subject.xlsx")
